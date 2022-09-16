80da9f951d8768ceea0289ca5d7f517103aaa096
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-16 12:42:21 -0300

    Projeto happy vinhos - cabecalho finalizado, produtos finalizado, diferenciais finalizado, planos finalizado. Trabalhando no carousel JS incompleto

--- design.js
+++ design.js
@@ -1,22 +1,34 @@
 const card = document.querySelectorAll('.slides');
 const next = document.querySelector('.next__btn');
 const prev = document.querySelector('.previous__btn');
-var pos = '0';
+let posicao = 0;
 
+slideShow();
 
-card[pos].style.cssText = 'display:block;';
+function slideShow(){
+    for(let i = 1; i<card.length ; i++){
+    card[i].style.cssText = 'display:none';
+}
 
     next.onclick = function(){
-        console.log("NEXT");
-        card[0].style.cssText = 'display:none;';
-        card[pos++].style.cssText = 'display:block;';
         
-    }
-    prev.onclick = function(){
-        console.log("PREVIOUS");
+        posicao++;
+        
+        if(posicao >= card.length){
+            posicao = 0;
+        }
+        if(posicao < card.length){
+        }
+        console.log(posicao);
+        card[posicao].style.cssText = 'display:block';
     }
 
-for(let i=0; i < card.length ; i++)
-{
-    console.log(card[i]);
+    prev.onclick = function(){
+        posicao--;
+        if(posicao < 0){
+            posicao = 4;
+        }
+        console.log(posicao);
+        card[posicao].style.cssText = 'display:block';
+    }
 }
