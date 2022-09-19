04a380a93c994b753a347d3e4281e8bb0732e0e6
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-19 11:04:59 -0300

    Projeto happy vinhos - carrousel finalzado. Proximo passo cabecalho

--- design.js
+++ design.js
@@ -1,34 +1,82 @@
 const card = document.querySelectorAll('.slides');
-const next = document.querySelector('.next__btn');
-const prev = document.querySelector('.previous__btn');
+const btn = document.querySelectorAll('.btn');
+let tam = card.length;
 let posicao = 0;
 
-slideShow();
+// PRIMEIRO PLANO A SER MOSTRADO
+card[posicao].style.cssText = ' opacity: 1;' + 
+'height: auto;';
 
-function slideShow(){
-    for(let i = 1; i<card.length ; i++){
-    card[i].style.cssText = 'display:none';
+btn.forEach( botao => {
+    botao.addEventListener('click', () => {
+        const next = botao.classList.contains("next__btn");
+        resetPlanos();
+        if(next){
+            // console.log("Direita"); Testando o botao selecionado
+            posicao++;
+        }
+        else{
+            // console.log("Esquerda"); Testando o botao selecionado
+            posicao--;
+        }
+
+        if(posicao >=tam){
+            posicao=0;
+        }
+        if(posicao <0){
+            posicao = tam-1;
+        }
+        // console.log(posicao); Teste da posicao selecionada
+        card[posicao].style.cssText =  ' opacity: 1;' + 
+        'height: auto;' + 
+        'overflow: hidden;' + 
+        'transition: opacity 0.6s ease-in;';
+    })
+    
+})
+
+function resetPlanos(){
+    for(let i=0;i<tam;i++){
+        // card[i].style.cssText = 'display:none';
+        card[i].style.cssText = ' opacity: 0;' + 
+        'height: 0;' + 
+        'overflow: hidden;' + 
+        'transition: opacity 0.6s ease-in;';
+    }
 }
 
-    next.onclick = function(){
-        
-        posicao++;
-        
-        if(posicao >= card.length){
-            posicao = 0;
-        }
-        if(posicao < card.length){
-        }
-        console.log(posicao);
-        card[posicao].style.cssText = 'display:block';
-    }
 
-    prev.onclick = function(){
-        posicao--;
-        if(posicao < 0){
-            posicao = 4;
-        }
-        console.log(posicao);
-        card[posicao].style.cssText = 'display:block';
-    }
-}
+
+
+
+
+
+// slideShow();
+
+// function slideShow(){
+//     for(let i = 1; i<card.length ; i++){
+//     card[i].style.cssText = 'display:none';
+// }
+
+//     next.onclick = function(){
+        
+//         posicao++;
+        
+//         if(posicao >= card.length){
+//             posicao = 0;
+//         }
+//         if(posicao < card.length){
+//         }
+//         console.log(posicao);
+//         card[posicao].style.cssText = 'display:block';
+//     }
+
+//     prev.onclick = function(){
+//         posicao--;
+//         if(posicao < 0){
+//             posicao = 4;
+//         }
+//         console.log(posicao);
+//         card[posicao].style.cssText = 'display:block';
+//     }
+// }
